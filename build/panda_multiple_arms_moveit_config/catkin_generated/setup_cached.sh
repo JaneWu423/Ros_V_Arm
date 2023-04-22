7f2452bb9ce57e13015df9873e9f7b4877d233e4
Jane Wu <jiayi.wu@vanderbilt.edu>
2023-04-22 22:21:20 +0000

    fixed v

--- build/panda_multiple_arms_moveit_config/catkin_generated/setup_cached.sh
+++ build/panda_multiple_arms_moveit_config/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/ubuntu/finalProject/devel/.private/panda_multiple_arms_moveit_config:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/ubuntu/finalProject/devel/.private/panda_multiple_arms_moveit_config/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/ubuntu/finalProject/devel/.private/panda_multiple_arms_moveit_config/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/ubuntu/finalProject/build/panda_multiple_arms_moveit_config'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/ubuntu/finalProject/devel/.private/panda_multiple_arms_moveit_config/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/ubuntu/finalProject/src/panda_multiple_arms_moveit_config:$ROS_PACKAGE_PATH"
