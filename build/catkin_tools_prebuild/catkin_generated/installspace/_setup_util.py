89762526cda38a5ad8294bb5ab66dcf02e1a90fe
Jane Wu <jiayi.wu@vanderbilt.edu>
2023-04-22 21:33:32 +0000

    planner configured but left/right arm does not work

--- build/catkin_tools_prebuild/catkin_generated/installspace/_setup_util.py
+++ build/catkin_tools_prebuild/catkin_generated/installspace/_setup_util.py
@@ -268,7 +268,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = r'/home/ubuntu/algorithm_of_robotics_workspace/devel;/home/ubuntu/catkin_ws/devel;/opt/ros/melodic'.split(';')
+            CMAKE_PREFIX_PATH = r'/home/ubuntu/finalProject/devel;/home/ubuntu/algorithm_of_robotics_workspace/devel;/home/ubuntu/catkin_ws/devel;/opt/ros/melodic'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
---
+++ devel/share/franka_description/cmake/franka_descriptionConfig.cmake
@@ -0,0 +1,1 @@
+/home/ubuntu/finalProject/devel/.private/franka_description/share/franka_description/cmake/franka_descriptionConfig.cmake
